e6aa7fb2fa866f925af7d6d47da5ee8210d71021
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-16 16:26:03 -0500

    basic random generating functions written

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+/*
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -15,7 +16,32 @@ function writePassword() {
   confirm("Do you require any special characters?")
 
 }
+*/
+
+//Generator functions
+
+//this will display a random number 0-9
+console.log(Math.floor(Math.random() * 9));
+
+function getRandomLower() {
+  const lettersLower = "abcdefghijklmnopqrstuvwxyz";
+  return lettersLower[Math.floor(Math.random() * lettersLower.length)];
+};
+console.log(getRandomLower());
+
+function getRandomUpper() {
+  const lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return lettersUpper[Math.floor(Math.random() * lettersUpper.length)];
+};
+console.log(getRandomUpper());
+
+function getRandomSymbol() {
+  const symbols = "!#$%&*/-+";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+console.log(getRandomSymbol());
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+//generateBtn.addEventListener("click", writePassword());
 
