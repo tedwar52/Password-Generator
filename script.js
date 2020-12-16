1adb0b9804e7a99cab8289a54caf577e7301192b
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-16 16:55:59 -0500

    modified writePassword function... starting generatePassword function... taking prompt input as a value. created variables for from prompts/confirm windows

--- script.js
+++ script.js
@@ -2,26 +2,36 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-/*
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  prompt("How many characters long would you like your password? (8-128characters)");
-  confirm("Does your password require special criteria?");
-  confirm("Do you require lowercase letters?");
-  confirm("Do you require uppercase letters?");
-  confirm("Do you require any numbers?");
-  confirm("Do you require any special characters?")
+}
+
+function generatePassword() {
+  var length = prompt("How many characters long would you like your password? (8-128characters)");
+  console.log(length);
+  
+  var criteria = confirm("Does your password require special criteria?");
+  if (criteria) {
+    var lower = confirm("Do you require lowercase letters?");
+    var upper = confirm("Do you require uppercase letters?");
+    var numbs = confirm("Do you require any numbers?");
+    var special = confirm("Do you require any special characters?");
+  }
+  
 
 }
-*/
 
 //Generator functions
-
 //this will display a random number 0-9
 console.log(Math.floor(Math.random() * 9));
+
+function getRandomNumber() {
+  const numbers = "0123456789";
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
 
 function getRandomLower() {
   const lettersLower = "abcdefghijklmnopqrstuvwxyz";
@@ -43,5 +53,5 @@ console.log(getRandomSymbol());
 
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword());
 
