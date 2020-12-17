cfb14d11b8cff6aabb4d5cf90e0b5f533131123e
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-16 23:41:40 -0500

    have results from generator functions connecting in a string

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pass = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -10,27 +11,45 @@ function writePassword() {
 }
 
 function generatePassword() {
-  const length = prompt("How many characters long would you like your password? (8-128characters)");
-  console.log(length);
+  const passLength = prompt("How many characters long would you like your password? (8-128characters)");
+  console.log(passLength);
   
   var criteria = confirm("Does your password require special criteria?");
   if (criteria) {
     var upper = confirm("Do you require uppercase letters?");
     if(upper === true) {
-      getRandomUpper();
+      pass += getRandomUpper();
+      pass += getRandomLower();
+      console.log("letters generated " + pass);
     } else {
-      getRandomLower();
+      pass += getRandomLower();
+      console.log("only lowercase letters generated " + pass);
     }
     var numbs = confirm("Do you require any numbers?");
     if(numbs === true) {
-      getRandomNumber();
+      pass += getRandomNumber();
+      console.log("numbers generated " + pass);
     }
     var special = confirm("Do you require any special characters?");
     if(special === true) {
-      getRandomSymbol();
+      pass += getRandomSymbol();
+      console.log("symbols generated " + pass);
     }
   }
 
+  /*
+  var password = ""
+  for (var i=0; i < passLength; i++) {
+    password = password +
+  }
+  */
+  
+  /*
+  var passwordLength = passlength;
+  for (var i=0; i < passwordLength.length; i++) {
+    pass +=
+  }
+  */
   /*
   const hasLower = lower === true;
   const hasUpper = upper === true;
