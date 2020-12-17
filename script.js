3a78ba29ae061fec9a50bd0f47b68b1ea990c5cf
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-17 00:31:16 -0500

    added notes to organize the code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// Assignment Code
+//-----------Defining variables-----------------------------------------
+
 var generateBtn = document.querySelector("#generate");
 var pass = "";
 var password = "";
 
-// Write password to the #password input
+//---------Write password to the #password input------------------------
+
 function writePassword() {
   generatePassword();
   var passwordText = document.querySelector("#password");
@@ -11,12 +13,16 @@ function writePassword() {
   passwordText.value = password;
 }
 
+//----------------Create the password-----------------------------------
+
 function generatePassword() {
+  //Prompt asking how long the password needs to be
   var passLength = prompt("How many characters long would you like your password? (8-128characters)");
   console.log(passLength);
   
   var criteria = confirm("Does your password require special criteria?");
   if (criteria) {
+    //Will ask a series of confirm messages if password needs special criteria and will add certain elements as needed
     var upper = confirm("Do you require uppercase letters?");
     if(upper === true) {
       pass += getRandomUpper();
@@ -39,17 +45,17 @@ function generatePassword() {
   }
   console.log(pass);
 
-  //--THIS RETURNS PASSWORD AS NUMERICAL ONLY IN THE CORRECT LENGTH
+  //Create a loop using the above collected criteria to string together a password to the desired length
   for (var i=0; i < passLength; i++) {
     password += pass.charAt(Math.floor(Math.random() * pass.length));
   }
   console.log(password);
-  
 
 }
 
 
-//Generator functions
+//---------------------Generator functions------------------------------------
+
 function getRandomNumber() {
   const numbers = "0123456789";
   return numbers[Math.floor(Math.random() * numbers.length)];
@@ -71,6 +77,7 @@ function getRandomSymbol() {
 };
 
 
-// Add event listener to generate button
+//---------------Add event listener to generate button---------------------------
+
 generateBtn.addEventListener("click", writePassword());
 
